8d824570af5142af85a48a00ca062cd5f4081766
damian.uzieblo@gmail.com <aplikacja>
2018-01-23 13:57:35 +0100

    - rozszerzyłem obrazek na ekranie głównym - skalowanie i wyświetlenie na ekranie głównym zrobionego zdjęcia z aparatu wbudowanego w urządzeniu - przejście do podglądu z możliwością skalowania zdjęcia po kliknięciu zdjęcia na głównym ekranie

--- app/src/main/java/com/duzieblo/example/takepicture/MainActivity.java
+++ app/src/main/java/com/duzieblo/example/takepicture/MainActivity.java
@@ -1,17 +1,19 @@
 package com.duzieblo.example.takepicture;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -29,38 +31,71 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String PHOTO_FILENAME = "photo.jpg";
 
-    @BindView(R.id.imageView) ImageView imageView;
-    @BindView(R.id.fabTakePhoto) FloatingActionButton fab;
+    @BindView(R.id.imageView) ImageView mImageView;
+    @BindView(R.id.fabTakePhoto) FloatingActionButton mFab;
 
     private Context mContext;
-    private Unbinder unbinder;
+    private Unbinder mUnbinder;
+
+    private File mPhotoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        unbinder = ButterKnife.bind(this);
+        mUnbinder = ButterKnife.bind(this);
 
 
         mContext = this;
+
+        Log.d(getClass().getName(), "onCreate() Szerokość obrazka: " + mImageView.getWidth());
+
+        mImageView.getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                Log.d(getClass().getName(), "Widok imageView został dołączony");
+                Log.d(getClass().getName(), "onWindowAttached - Szerokość obrazka: " + mImageView.getWidth());
+                mImageView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+            }
+
+        });
+
+        Log.d(getClass().getName(), "Koniec wywołania metody onCreate()");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        unbinder.unbind();
+        mUnbinder.unbind();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_PHOTO) {
+            if (resultCode == Activity.RESULT_OK) {
+                Uri uri = FileProvider.getUriForFile(this,
+                        "com.duzieblo.example.takepicture.fileprovider",
+                        mPhotoFile);
+                revokeUriPermission(uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                if (mPhotoFile != null && mPhotoFile.exists()) {
+                    Bitmap bitmap = PictureUtils.getScaledBitmap(mPhotoFile.getPath(),
+                            this);
+                    mImageView.setImageBitmap(bitmap);
+                }
+            }
+        }
     }
 
     @OnClick(R.id.fabTakePhoto)
     public void takePhoto() {
-        Log.d(getClass().getName(), "FAB click");
-        File photoFile = getPhotoFile(PHOTO_FILENAME);
+        Log.d(getClass().getName(), "FAB click" + mImageView.getWidth());
+        mPhotoFile = getPhotoFile(PHOTO_FILENAME);
         final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-        if (photoFile != null && canTakePhoto(captureImage)) {
+        if (mPhotoFile != null && canTakePhoto(captureImage)) {
             Uri uri = FileProvider.getUriForFile(this,
                     "com.duzieblo.example.takepicture.fileprovider",
-                    photoFile);
+                    mPhotoFile);
             captureImage.putExtra(MediaStore.EXTRA_OUTPUT, uri);
 
             //zwraca aktywności które obsługują intencję robienia zdjęć
@@ -79,6 +114,28 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @OnClick(R.id.imageView)
+    public void showPhoto() {
+        if (mPhotoFile != null && mPhotoFile.exists()) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setType("image/jpg");
+            Uri uri = FileProvider.getUriForFile(this,
+                    "com.duzieblo.example.takepicture.fileprovider",
+                    mPhotoFile);
+            intent.setData(uri);
+            List<ResolveInfo> previewPhotoActivities = getPackageManager().
+                    queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+            for (ResolveInfo activity : previewPhotoActivities) {
+                //przydzielenie uprawnienia do zapisu pod wskazanym URI dla aktywnosci,
+                //które obsługują robienia zdjęć
+                grantUriPermission(activity.activityInfo.packageName,
+                        uri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            }
+            startActivity(intent);
+        }
+
+    }
+
     private File getPhotoFile(String name) {
         File filesDir = mContext.getFilesDir();
         return new File(filesDir, name);
@@ -86,6 +143,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Metoda sprawdza czy istnieje jakiekolwiek aktywność która obsługuję robienie zdjęcia.
+     *
      * @param intent obiekt intencji robienia zdjęcia.
      * @return
      */
--- app/src/main/java/com/duzieblo/example/takepicture/PictureUtils.java
+++ app/src/main/java/com/duzieblo/example/takepicture/PictureUtils.java
@@ -1,7 +1,10 @@
 package com.duzieblo.example.takepicture;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.util.Log;
 
 /**
  * @author duzieblo
@@ -12,10 +15,14 @@ public class PictureUtils {
     public static Bitmap getScaledBitmap(String path, int destWidth, int destHeight) {
         //Odczytywanie rozmiarów obrazu z pliku na dysku
         BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
+        options.inJustDecodeBounds = true; // nie zwraca bitmapy tylko opcje
         BitmapFactory.decodeFile(path, options);
         float srcWidth = options.outWidth;
         float srcHeight = options.outHeight;
+        Log.d(PictureUtils.class.getName(), "Szerokość zdjęcia: " + srcWidth);
+        Log.d(PictureUtils.class.getName(), "Wysokość zdjęcia: " + srcHeight);
+        Log.d(PictureUtils.class.getName(), "Szerokość parametru: " + destWidth);
+        Log.d(PictureUtils.class.getName(), "Wysokość parametru: " + destHeight);
 
         //Określanie współczynnika przeskalowania
         int inSampleSize = 1;
@@ -23,12 +30,21 @@ public class PictureUtils {
             float heightScale = srcHeight / destHeight;
             float widthScale = srcWidth / destWidth;
             inSampleSize = Math.round(heightScale > widthScale ? heightScale : widthScale);
+            Log.d(PictureUtils.class.getName(), "inSampleSize: " + inSampleSize);
         }
 
         options = new BitmapFactory.Options();
+        //określenie jak duża powinna być próbka pikseli
+        // oryginału na każdy piksel przeskalowanej bitmapy
         options.inSampleSize = inSampleSize;
 
         //Odczytywanie pliku i tworzenie finalnej bitmapy
         return BitmapFactory.decodeFile(path, options);
+    }
+
+    public static Bitmap getScaledBitmap(String path, Activity activity) {
+        Point size = new Point();
+        activity.getWindowManager().getDefaultDisplay().getSize(size);
+        return getScaledBitmap(path, size.x, size.y);
     }
 }
